2cb625ee06c3a006aac38984b41d8f232b18825a
Louis Delacroix <yoursecretsaresafe@googlemail.com>
2011-04-12 03:20:34 +0100

    Implemented map time. Refactored light map generation a bit, though light spreading is disabled for now.

--- src/map.h
+++ src/map.h
@@ -12,7 +12,7 @@
 class Map
 {
 public:
-  Map();
+  Map(unsigned long long int ticks);
 
   inline bool haveChunk(const ChunkCoords & cc) const { return m_chunkMap.count(cc) > 0; }
 
@@ -21,10 +21,12 @@ public:
   inline       Chunk & chunk(const WorldCoords & wc)       { return *(m_chunkMap.find(getChunkCoords(wc))->second); }
   inline const Chunk & chunk(const WorldCoords & wc) const { return *(m_chunkMap.find(getChunkCoords(wc))->second); }
 
-  /// If no chunk exists at cc, create a random one. Always returns the chunk at cc.
-  Chunk & getChunkOrGenerateNew(const ChunkCoords & cc);
+  /// If no chunk exists at cc, load from disk or create a random one if none exists.
+  void ensureChunkIsLoaded(const ChunkCoords & cc);
 
   inline void insertChunk(std::shared_ptr<Chunk> chunk) { m_chunkMap.insert(ChunkMap::value_type(chunk->coords(), chunk)); }
+
+  unsigned long long int tick_counter;
 
 private:
   ChunkMap m_chunkMap;
